e8d0c64d17e5663dea7b4b1427e4f69307b50061
Maria <synevicmaria13@gmail.com>
2026-03-09 20:29:10 +0200

    style: MainHome

--- frontend/src/components/MainHome.jsx
+++ frontend/src/components/MainHome.jsx
@@ -1,28 +1,27 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import ico from "../assets/favicon.ico";
-import '../css/MainHome.css';
+import "../css/MainHome.css";
 
 function MainHome() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <main>
-        <section className='section1'>
-          <div>
-            <img src={ico} alt='icoKurmi'/>
-          </div>
-          <div className='line'>
-            </div>
-          <button 
-            onClick={() => navigate('/quiz')} 
-            className='navigate-button'
-          >
+    <main>
+      <section className="section1">
+        <div className="block1">
+          <img className="ico" src={ico} alt="icoKurmi" />
+        </div>
+
+        <div className="line">
+          <button onClick={() => navigate("/quiz")} className="quiz-daily-word">
             Daily word
           </button>
-        </section>
-      </main>
-    </div>
+          <button onClick={() => navigate("/quiz")} className="random-word">
+            Random
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
 
