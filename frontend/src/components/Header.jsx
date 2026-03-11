5ca83816b659d0c773034ff69f871ad4981651b8
XxMariavxX <synevicmaria13@gmail.com>
2026-03-11 17:40:32 +0200

    es modules

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import "../css/Header.css";
-import ico from "../assets/favicon.ico";
+import carrot from "../assets/carrot-before-hovers.png"
+
 
 function Header() {
   return (
     <div>
       <header className="header">
         <div className="logo-block">
-          <a>
-            <img src={ico} alt="logo" className="logo-header" />
-          </a>
+          <nav className="nav">
+            <a className="help-carrot">
+              <img src = {carrot} alt="help-carrot"/>
+            </a>
+          </nav>
         </div>
       </header>
     </div>
