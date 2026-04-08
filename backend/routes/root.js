1376487afad140c2b5d05f660b73a37f4029c99d
XxMariavxX <synevicmaria13@gmail.com>
2026-04-09 01:28:57 +0300

    hint route

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -1,16 +1,17 @@
 import { getDailyWordMeta, checkGuess } from "../controllers/wordController.js";
+import { newFilter, checkWordMatchSlowly } from "../lib/asyncHelpers.js";
+import fiveLetterWords from ".../words/fiveLetterWords.js";
 
 export default async function (fastify, opts) {
-
-  fastify.get('/daily-word', async function (request, reply) {
+  fastify.get("/daily-word", async function (request, reply) {
     return getDailyWordMeta();
   });
 
-  fastify.post('/check-word', async function (request, reply) {
+  fastify.post("/check-word", async function (request, reply) {
     const { guess } = request.body;
 
-    if (!guess || typeof guess !== 'string') {
-      return reply.code(400).send({ error: 'Invalid guess: must be a string' });
+    if (!guess || typeof guess !== "string") {
+      return reply.code(400).send({ error: "Invalid guess: must be a string" });
     }
 
     try {
@@ -18,6 +19,30 @@ export default async function (fastify, opts) {
       return result;
     } catch (error) {
       return reply.code(400).send({ error: error.message });
+    }
+  });
+
+  fastify.get("/hints", async function (request, reply) {
+    const { letter } = request.query;
+
+    if (!letter || typeof letter !== "string") {
+      return reply.code(400).send({ error: "Choose the letter for tip" });
+    }
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const timeOutId = setTimeout(() => {
+      controller.abort();
+    }, 3000);
+
+    try {
+      const matchingWords = await newFilter(
+        fiveLetterWords,
+        (word, sig) => checkWordMatchSlowly(word, letter, sig), signal
+      );
+
+      clearTimeout(timeOutId);
     }
   });
 }
